e07136afa8d02466c9a7cf4db311000fa54376b0
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2024-04-02 02:44:07 +0000

    adding certifictes to `database.js`

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObj) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
   await client.connect();
   try {
@@ -19,6 +19,16 @@ async function query(queryObj) {
     await client.end();
   }
 }
+
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
