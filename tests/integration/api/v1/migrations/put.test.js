db0f81a701fffc3b043f49d96ac381a81c9a4d35
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2024-06-07 20:26:09 +0000

    Makes `npm test` more robust and adds `orchestrator.js`

--- tests/integration/api/v1/migrations/put.test.js
+++ tests/integration/api/v1/migrations/put.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("PUT to api/v1/migrations should return 405", async () => {
   let response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "PUT",
