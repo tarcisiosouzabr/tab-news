c39635e3389b50bd1dacbf5cb2ef03b538295505
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2025-03-13 01:13:08 +0000

    feat: adds `InternalServerError` and fixes issue with `database.js`

---
+++ infra/error.js
@@ -0,0 +1,19 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause: cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      name: this.name,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
