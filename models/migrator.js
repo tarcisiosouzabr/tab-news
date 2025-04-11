fd8514d31c86c88e6b01a37b815e157a6e88aa03
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2025-04-11 20:56:17 +0000

    feat: adds `models/migrator.js` and use on `api/v1/migrations`

---
+++ models/migrator.js
@@ -0,0 +1,49 @@
+import database from "infra/database";
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: true,
+      dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigration() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+const migrator = {
+  listPendingMigrations,
+  runPendingMigration,
+};
+
+export default migrator;
