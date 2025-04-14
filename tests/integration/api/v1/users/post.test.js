8d557ca2fa9a6981978be68167ace9aa3ed3e306
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2025-04-14 21:55:16 +0000

    feat: adds `create-user.migration` and `post.test` for `api/v1/migration`

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,18 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST to api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
