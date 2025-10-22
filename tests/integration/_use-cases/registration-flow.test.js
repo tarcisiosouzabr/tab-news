1977bda0c8fcb49a7e3dae822fd0a0a7c33f6944
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2025-10-22 02:38:11 +0000

    feat: adds migration features

---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,44 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("Use case: Registration Flow (all successful)", () => {
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "RegistrationFlow",
+          email: "registration.flow@curso.dev",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+
+    expect(createUserResponse.status).toBe(200);
+
+    const createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponse).toEqual({
+      id: createUserResponseBody.id,
+      username: "RegistrationFlow",
+      email: "registration.flow@curso.dev",
+      password: createUserResponseBody.password,
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+  test("Receive activation email", async () => {});
+  test("Activate account", async () => {});
+  test("Login", async () => {});
+  test("Get User", async () => {});
+});
