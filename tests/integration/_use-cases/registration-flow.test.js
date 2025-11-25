55b32144e49e550ec1d5a6a346f40be596e335c3
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2025-11-25 15:53:50 +0000

    feat: adds middlewares for check features

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,7 +1,7 @@
-import orchestrator from "tests/orchestrator";
-import activation from "models/activation";
+import orchestrator from "tests/orchestrator.js";
+import activation from "models/activation.js";
 import webserver from "infra/webserver.js";
-import user from "models/user";
+import user from "models/user.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -80,6 +80,24 @@ describe("Use case: Registration Flow (all successful)", () => {
     const activatedUser = await user.findOneByUsername("RegistrationFlow");
     expect(activatedUser.features).toEqual(["create:session"]);
   });
-  test("Login", async () => {});
+  test("Login", async () => {
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@curso.dev",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+    expect(createSessionResponse.status).toBe(201);
+
+    const createSessionResponseBody = await createSessionResponse.json();
+    expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
+  });
   test("Get User", async () => {});
 });
