43c1cb62d7c7f8754a41dab5f96e788683efdb36
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2026-01-06 03:41:10 +0000

    feat: adds `canRequest()` to user index.js post

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -4,7 +4,8 @@ import user from "models/user.js";
 import activation from "models/activation.js";
 
 const router = createRouter();
-router.post(postHandler);
+router.use(controller.injectAnonymousOrUser);
+router.post(controller.canRequest("create:user"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 
